fd5a426d4d4be3d77ab2552946a4fa51993f50c0
Reddy M <deva.reddy-m@capgemini.com>
2019-11-04 17:13:13 +0530

    first commit

---
+++ src/main/java/com/employee/emplpoyee/EmplpoyeeApplication.java
@@ -0,0 +1,12 @@
+package com.employee.emplpoyee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class EmplpoyeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmplpoyeeApplication.class, args);
+	}
+
+}
